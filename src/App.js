b322d1fe072ea450e630a3c6d3ee9ffcca4096f4
Eve Stockstill <evestockstillpdx@gmail.com>
2020-04-18 19:42:49 -0700

    custom form hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from './hooks/useForm';
+
+
+
+// const App = () => {
+//   const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+//   return (
+//     <>
+//       <div>
+//         <button
+//           onClick={() =>
+//             setCount((currentState) => ({
+
+//               count: currentState.count + 1,
+//               count2: currentState.count2 + 1
+//             }))
+//           }
+//         >
+//           increase
+//         </button>
+//       </div>
+//       <div>count 1: {count}</div>
+//       <div>count 2: {count2}</div>
+//     </>
+//   );
+// };
+
 
 const App = () => {
-  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+  const [values, handleChange] = useForm({ email: '', password: '' });
+ 
   return (
-    <>
-      <div>
-        <button
-          onClick={() =>
-            setCount((currentState) => ({
-              ...currentState,
-              count: currentState.count + 1
-            }))
-          }
-        >
-          increase
-        </button>
-      </div>
-      <div>count 1: {count}</div>
-      <div>count 2: {count2}</div>
-    </>
+    <div>
+      <input name='email' value={values.email} onChange={handleChange}/>
+      <input type='password' value={values.password} name='password' onChange={handleChange}/>
+    </div>
   );
 };
 
---
+++ src/hooks/useForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export const useForm = (initValues) => {
+  const [values, setValues] = useState(initValues);
+
+  return [
+    values,
+    (e) => {
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value,
+      });
+    },
+  ];
+};
